02e692a97a4d0bdd6e1a028da96e0b3a02a26a98
Karl Bruder <karl.j.bruder@gmail.com>
2020-07-24 14:01:13 +0900

    Taskbox UI

---
+++ storybook-static/precache-manifest.5eddbba764f1a975de096e96c71c2bb9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d533db1babc466b2fcdd781847dad106",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.c62998a7557e3f577083.bundle.js"
+  },
+  {
+    "url": "runtime~main.c62998a7557e3f577083.bundle.js"
+  },
+  {
+    "revision": "10e2afec26b011598147",
+    "url": "static/css/main.67471bcc.chunk.css"
+  },
+  {
+    "url": "vendors~main.c62998a7557e3f577083.bundle.js"
+  },
+  {
+    "url": "vendors~main.c62998a7557e3f577083.bundle.js.LICENSE.txt"
+  }
+]);
